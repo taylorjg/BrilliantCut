e89709b53597db995bd616855514d1d877ef16be
Jon Taylor <jont@cakesolutions.net>
2017-09-01 00:19:56 +0100

    A bit of simplification

--- src/brilliantCut.js
+++ src/brilliantCut.js
@@ -2,8 +2,8 @@
 
 const R = require('ramda');
 
-const maxBy = (xs, f) => xs.reduce((acc, x) => f(x) > f(acc) ? x : acc);
-
+const max = xs => xs.reduce((acc, x) => Math.max(acc, x));
+// const maxBy = (xs, f) => xs.reduce((acc, x) => f(x) > f(acc) ? x : acc);
 const sum = xs => xs.reduce((a, b) => a + b, 0);
 
 function* calculateCombinations(chunkSize, availableCuts, currentCuts) {
@@ -47,14 +47,15 @@ const processGemType = (input, gemType) => {
     );
 };
 
+const GEM_TYPES = ['diamond', 'sapphire', 'ruby'];
+
 const largestProfit = input => {
-    const xss1 = processGemType(input, 'diamond');
-    const xss2 = processGemType(input, 'sapphire');
-    const xss3 = processGemType(input, 'ruby');
-    const v1 = xss1.map(xs => maxBy(xs, x => x.profit)).map(x => x.profit);
-    const v2 = xss2.map(xs => maxBy(xs, x => x.profit)).map(x => x.profit);
-    const v3 = xss3.map(xs => maxBy(xs, x => x.profit)).map(x => x.profit);
-    return sum(v1) + sum(v2) + sum(v3);
+    const v2 = GEM_TYPES
+        .map(R.curry(processGemType)(input))
+        .map(allChunkCombinations =>
+            allChunkCombinations.map(chunkCombinations =>
+                max(chunkCombinations.map(combination => combination.profit))));
+    return sum(v2.map(xs => sum(xs)));
 };
 
 module.exports = {
