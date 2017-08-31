db122a442a9e7352616e8beca9354f321f7f059c
Jon Taylor <jont@cakesolutions.net>
2017-08-31 23:18:00 +0100

    Introduce Ramda

--- src/brilliantCut.js
+++ src/brilliantCut.js
@@ -1,28 +1,32 @@
 'use strict';
 
-function* calculateCombinations(chunkSize, cuts, currentCuts) {
+const R = require('ramda');
+
+function* calculateCombinations(chunkSize, availableCuts, currentCuts) {
     currentCuts = currentCuts || [];
-    for (let i = 0; i < cuts.length; i++) {
-        const cut = cuts[i];
+    for (let i = 0; i < availableCuts.length; i++) {
+        const cut = availableCuts[i];
         const remainingChunkSize = chunkSize - cut.size;
         if (remainingChunkSize > 0) {
             const clonedCurrentCuts = currentCuts.slice();
             clonedCurrentCuts.push(cut);
             yield clonedCurrentCuts;
-            yield* calculateCombinations(remainingChunkSize, cuts, clonedCurrentCuts);
+            yield* calculateCombinations(remainingChunkSize, availableCuts, clonedCurrentCuts);
         }
     }
 }
 
-const calculateDetails = (rawChunkSize, cuts) => {
+const calculateDetails = (chunkSize, cuts) => {
 
-    const value = sum(cuts.map(x => x.value));
-    const sumOfCutSizes = sum(cuts.map(x => x.size));
-    const waste = rawChunkSize - sumOfCutSizes;
+    const cutValues = cuts.map(cut => cut.value);
+    const cutSizes = cuts.map(cut => cut.size);
+    const value = sum(cutValues);
+    const sumOfCutSizes = sum(cutSizes);
+    const waste = chunkSize - sumOfCutSizes;
     const profit = value - waste;
 
     return {
-        cuts: cuts.map(x => x.size),
+        cuts: cutSizes,
         value,
         waste,
         profit
@@ -35,12 +39,12 @@ const sum = xs => xs.reduce((a, b) => a + b);
 const process = input => {
     const rawChunkSize = input.diamond.rawChunks[0];
     const cuts = input.diamond.cuts;
-    const combinationsOfCuts = Array.from(calculateCombinations(rawChunkSize, cuts));
-    const cutDetails = combinationsOfCuts.map(currentCuts => calculateDetails(rawChunkSize, currentCuts));
-    return cutDetails;
+    return R.uniq(Array.from(calculateCombinations(rawChunkSize, cuts)))
+        .map(R.curry(calculateDetails)(rawChunkSize));
 };
 
-const largestProfit = input => maxBy(process(input), e => e.profit).profit;
+const largestProfit = input =>
+    maxBy(process(input), details => details.profit).profit;
 
 module.exports = {
     process,
